199632f3417fee7d7a5c553b117efcc38954b756
Vardhman Mehta <vardhman.mehta96@gamil.com>
2015-03-02 02:35:08 -0800

    Added bootstrap

--- script.js
+++ script.js
@@ -7,8 +7,11 @@ var calculator = function() {
 
 	var print = [];
 
+	var count = 0;
+
 	for (var i = 2; i < num; i++) {
 		if($.inArray(i, numbers) == -1) {
+			count++;
 			print.push(i);
 		} 
 		for (var j = i ; j < num; j+=i) {
@@ -19,6 +22,10 @@ var calculator = function() {
 	for (var i = 0; i < print.length; i++) {
 		output+='<li>' + print[i] + '</li>';
 	};
+
+	var updateCount = document.getElementById('count');
+	updateCount.innerHTML = "<br>There are " + count + " prime numbers between 0 and " + num;
+
 	var update = document.getElementById('result');
 	update.innerHTML = output;
 };
